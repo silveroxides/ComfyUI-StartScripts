2693baecc94520e85e379606ef05467b0993f477
Silver <65376327+silveroxides@users.noreply.github.com>
2025-02-14 05:21:22 +0100

    Update comfyui-start.bat

--- comfyui-start.bat
+++ comfyui-start.bat
@@ -1,7 +1,10 @@
 rem @echo off
 
+rem Path to the folder containing your python.exe ececutable if it is not already in the Path environment variable.
 rem set PYTHON=
-rem set GIT=
+rem Path to the folder containing your git.exe executable if it is not already in the Path environment variable.
+rem set GIT_PATH=
+rem Set this to the path of any existing ComfyUI venv if you prefer to use that
 set VENV_DIR=
 set COMMANDLINE_ARGS=--windows-standalone
 
